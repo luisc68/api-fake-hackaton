22059a2f4e0fc1e0484e78132e14fad67c888dd7
luisc68 <j.cerna@unadeca.net>
2023-08-08 11:57:29 -0600

    "Add axios"

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+app.use(cors());
 
 app.get('/api/player/:playerId/:field?', (req, res) => {
   const playerId = parseInt(req.params.playerId);
